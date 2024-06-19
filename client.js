4c8d315d317ad3d9534562f10cf216f172945925
l4zy9uy <hoanghuudon02hp@gmail.com>
2024-06-19 16:48:53 +0700

    feat: add TLS when sending email to SMTP server

--- client.js
+++ client.js
@@ -1,8 +1,11 @@
 const net = require('net');
 const dns = require('dns');
 const os = require('os');
+const tls = require('tls');
+const { promisify } = require('util');
+
 const remoteAddress = process.argv[2];
-const port = 1025; // you can customize this directly or pass argument
+const port = 587; // you can customize this directly or pass argument
 const receiverEmail = process.argv[3];
 
 // Regular expressions for IP address and URL validation
@@ -55,31 +58,20 @@ if(receiverEmail === null) {
 }
 
 const localIPAddress = getLocalIpAddress();
-const senderEmail = `user@[${localIPAddress}]`;
+const senderEmail = `hoanghuudon02hp@gmail.com`;
 const dataMessage = "This is a test email.";
 const messages = [
-    `HELO ${localIPAddress}\r\n`,
+    `AUTH LOGIN\r\n`,
+    `${process.env.GG_USERNAME}\r\n`,
+    `${process.env.GG_PASSWORD}\r\n`,
     `MAIL FROM:<${senderEmail}>\r\n`,
     `RCPT TO:<${receiverEmail}>\r\n`,
     `DATA\r\n`,
-    `Subject: Test Email\r\nFrom: ${senderEmail}\r\nTo: 
-    ${receiverEmail}\r\n\r\n${dataMessage}\r\n.\r\n`,
+    `Subject: Test Email\r\nFrom: ${senderEmail}\r\nTo: ${receiverEmail}\r\n\r\n${dataMessage}\r\n.\r\n`,
     `QUIT\r\n`
 ];
 
-function getLocalIpAddress() {
-    const interfaces = os.networkInterfaces();
-    for (const interfaceName in interfaces) {
-        const addresses = interfaces[interfaceName];
-        for (const addressInfo of addresses) {
-            if (addressInfo.family === 'IPv4' && !addressInfo.internal) {
-                //console.log(`Local IP address: ${addressInfo.address}`);
-                return addressInfo.address;
-            }
-        }
-    }
-    console.log('No external IPv4 address found');
-}
+const client = new net.Socket();
 
 if (ipv4Regex.test(remoteAddress)) {
     console.log(`${remoteAddress} is an IPv4 remoteAddress.`);
@@ -101,18 +93,64 @@ if (ipv4Regex.test(remoteAddress)) {
     is neither a valid IP remoteAddress nor a URL.`);
 }
 
-const client = new net.Socket();
+
 client.connect(port, remoteAddress, () => {
     console.log(`connected to ${remoteAddress}:${port}`);
+    client.write('EHLO localhost\r\n');
 });
 
 client.on('data', (data) => {
-    console.log(`Received: ${data.toString()}`);
+    console.log(`Received:\n ${data.toString()}`);
 
-    if(messages.length !== 0) {
-        const message = messages.shift();
-        console.log(message);
-        client.write(message);
+    if (data.toString().includes('250-STARTTLS')) {
+        client.write('STARTTLS\r\n');
+    } else if (data.toString().includes('220 2.0.0 Ready to start TLS')) {
+        // Assume STARTTLS has been issued and server responded with '220 2.0.0 Ready to start TLS'
+        const secureContext = tls.createSecureContext({minVersion: 'TLSv1.2'});
+        const tlsSocket = new tls.TLSSocket(client, {
+            secureContext,
+            isServer: false
+        });
+
+        tlsSocket.connect({host: remoteAddress, port: port}, () => {
+            console.log("Start connect");
+            tlsSocket.write('EHLO localhost\r\n');
+        });
+        if(!tlsSocket.encrypted) {
+            console.log("Start TLS encryption");
+            tlsSocket.start();
+        }
+        tlsSocket.on('secureConnect', () => {
+            console.log('TLS connection secured');
+            tlsSocket.write('EHLO localhost\r\n');
+        });
+
+        tlsSocket.on('data', (data) => {
+            console.log('Received over TLS:\n', data.toString());
+            // Process server responses and send further commands as needed
+            if (messages.length > 0) { // Check for completion of EHLO response
+                // Now proceed to authenticate or send MAIL FROM, etc.
+                const message = messages.shift();
+                console.log(`Sending: ${message}`);
+                tlsSocket.write(message);
+                //console.log("Start sending");
+            }
+        });
+
+        tlsSocket.on('close', () => {
+            console.log('TLS Connection closed');
+        });
+
+        tlsSocket.on('error', (err) => {
+            console.error('TLS Socket Error:', err);
+        });
+    }
+    else {
+        /*if(messages.length !== 0) {
+            const message = messages.shift();
+            console.log(message);
+            client.write(message);
+        }*/
     }
 });
 
@@ -123,3 +161,17 @@ client.on('close', () => {
 client.on('error', (err) => {
     console.error(`Connection error: ${err.message}`);
 });
+
+function getLocalIpAddress() {
+    const interfaces = os.networkInterfaces();
+    for (const interfaceName in interfaces) {
+        const addresses = interfaces[interfaceName];
+        for (const addressInfo of addresses) {
+            if (addressInfo.family === 'IPv4' && !addressInfo.internal) {
+                //console.log(`Local IP address: ${addressInfo.address}`);
+                return addressInfo.address;
+            }
+        }
+    }
+    console.log('No external IPv4 address found');
+}
