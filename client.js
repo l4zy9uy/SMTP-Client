ef345e16330e11a3e09cb62e3e41123200b0c463
l4zy9uy <hoanghuudon02hp@gmail.com>
2024-06-19 19:58:51 +0700

    refactor: remove redundant lines of code and comments

--- client.js
+++ client.js
@@ -1,22 +1,19 @@
 const net = require('net');
-const dns = require('dns');
 const os = require('os');
 const tls = require('tls');
-const { promisify } = require('util');
-const regexUtil = require('./regexUtilities');
 
 const remoteAddress = process.argv[2];
-const port = 587; // you can customize this directly or pass argument
 const receiverEmail = process.argv[3];
+const port = process.env.PORT;
 
 if(remoteAddress === null || receiverEmail === null) {
-    console.log("You need to pass remoteAddress and receiver Email");
+    console.log("You need to pass remote Address and receiver Email");
     process.exit(1);
 }
 
-const localIPAddress = getLocalIpAddress();
 const senderEmail = `hoanghuudon02hp@gmail.com`;
 const dataMessage = "This is a test email.";
+
 const messages = [
     `AUTH LOGIN\r\n`,
     `${Buffer.from(process.env.GG_USERNAME).toString('base64')}\r\n`,
@@ -24,31 +21,15 @@ const messages = [
     `MAIL FROM:<${senderEmail}>\r\n`,
     `RCPT TO:<${receiverEmail}>\r\n`,
     `DATA\r\n`,
-    `Subject: Test Email\r\nFrom: ${senderEmail}\r\nTo: ${receiverEmail}\r\n\r\n${dataMessage}\r\n.\r\n`,
+    `Subject: Test Email
+    \r\nFrom: ${senderEmail}
+    \r\nTo: ${receiverEmail}
+    \r\n\r\n${dataMessage}
+    \r\n.\r\n`,
     `QUIT\r\n`
 ];
 
 const client = new net.Socket();
-
-if (regexUtil.ipv4Regex.test(remoteAddress)) {
-    console.log(`${remoteAddress} is an IPv4 remoteAddress.`);
-} else if (regexUtil.ipv6Regex.test(remoteAddress)) {
-    console.log(`${remoteAddress} is an IPv6 remoteAddress.`);
-} else if (regexUtil.urlRegex.test(remoteAddress)) {
-    console.log(`${remoteAddress} is a URL.`);
-    dns.resolve4(remoteAddress,
-        (err, remoteAddresses) => {
-        if (err) {
-            console.error(`Error resolving URL: ${err}`);
-        } else {
-            console.log(`IP remoteAddresses for 
-            ${remoteAddress}: ${remoteAddresses}`);
-        }
-    });
-} else {
-    console.log(`${remoteAddress} 
-    is neither a valid IP remoteAddress nor a URL.`);
-}
 
 
 client.connect(port, remoteAddress, () => {
@@ -90,7 +71,6 @@ client.on('data', (data) => {
                 const message = messages.shift();
                 console.log(`Sending: ${message}`);
                 tlsSocket.write(message);
-                //console.log("Start sending");
             }
         });
 
@@ -118,17 +98,3 @@ client.on('close', () => {
 client.on('error', (err) => {
     console.error(`Connection error: ${err.message}`);
 });
-
-function getLocalIpAddress() {
-    const interfaces = os.networkInterfaces();
-    for (const interfaceName in interfaces) {
-        const addresses = interfaces[interfaceName];
-        for (const addressInfo of addresses) {
-            if (addressInfo.family === 'IPv4' && !addressInfo.internal) {
-                //console.log(`Local IP address: ${addressInfo.address}`);
-                return addressInfo.address;
-            }
-        }
-    }
-    console.log('No external IPv4 address found');
-}
