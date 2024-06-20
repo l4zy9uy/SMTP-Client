247541ebfdd79f29adddd3faea02af77f17e6855
l4zy9uy <hoanghuudon02hp@gmail.com>
2024-06-20 09:38:08 +0700

    feat: can both send to normal server and server requiring TLS

--- client.js
+++ client.js
@@ -1,93 +1,54 @@
 const net = require('net');
-const os = require('os');
+const fs = require('fs');
 const tls = require('tls');
 
 const remoteAddress = process.argv[2];
-const receiverEmail = process.argv[3];
-const port = process.env.PORT;
+const senderEmail = process.argv[3];
+const receiverEmail = process.argv[4];
+const subject = "Test Email";
+const port = process.env.PORT_MAILHOG;
 
-if(remoteAddress === null || receiverEmail === null) {
-    console.log("You need to pass remote Address and receiver Email");
+if (remoteAddress === null || receiverEmail === null || senderEmail === null) {
+    console.log("You need to pass remote Address, sender Email and receiver" +
+        " Email");
     process.exit(1);
 }
 
-const senderEmail = `hoanghuudon02hp@gmail.com`;
-const dataMessage = "This is a test email.";
+let dataMessage;
+let messages;
+const client = new net.Socket();
 
-const messages = [
-    `AUTH LOGIN\r\n`,
-    `${Buffer.from(process.env.GG_USERNAME).toString('base64')}\r\n`,
-    `${Buffer.from(process.env.GG_PASSWORD).toString('base64')}\r\n`,
-    `MAIL FROM:<${senderEmail}>\r\n`,
-    `RCPT TO:<${receiverEmail}>\r\n`,
-    `DATA\r\n`,
-    `Subject: Test Email
+fs.readFile('./message', (err, data) => {
+    dataMessage = data.toString('utf-8');
+    messages = [
+        `EHLO localhost\r\n`,
+        `MAIL FROM:<${senderEmail}>\r\n`,
+        `RCPT TO:<${receiverEmail}>\r\n`,
+        `DATA\r\n`,
+        `Subject: ${subject}
     \r\nFrom: ${senderEmail}
     \r\nTo: ${receiverEmail}
     \r\n\r\n${dataMessage}
     \r\n.\r\n`,
-    `QUIT\r\n`
-];
+        `QUIT\r\n`
+    ];
 
-const client = new net.Socket();
-
-
-client.connect(port, remoteAddress, () => {
-    console.log(`connected to ${remoteAddress}:${port}`);
-    client.write('EHLO localhost\r\n');
-});
-
+    client.connect(port, remoteAddress, () => {
+        console.log(`Connected to ${remoteAddress}:${port}`);
+        //client.write('EHLO localhost\r\n');
+    });
+})
+let hasTLS = false;
 client.on('data', (data) => {
-    console.log(`Received:\n ${data.toString()}`);
+    console.log(`Received:\n${data.toString()}`);
 
     if (data.toString().includes('250-STARTTLS')) {
+        hasTLS = true;
         client.write('STARTTLS\r\n');
     } else if (data.toString().includes('220 2.0.0 Ready to start TLS')) {
-        // Assume STARTTLS has been issued and server responded with '220 2.0.0 Ready to start TLS'
-        const secureContext = tls.createSecureContext({minVersion: 'TLSv1.2'});
-        const tlsSocket = new tls.TLSSocket(client, {
-            secureContext,
-            isServer: false
-        });
-
-        tlsSocket.connect({host: remoteAddress, port: port}, () => {
-            console.log("Start connect");
-            tlsSocket.write('EHLO localhost\r\n');
-        });
-        if(!tlsSocket.encrypted) {
-            console.log("Start TLS encryption");
-            tlsSocket.start();
-        }
-        tlsSocket.on('secureConnect', () => {
-            console.log('TLS connection secured');
-            tlsSocket.write('EHLO localhost\r\n');
-        });
-
-        tlsSocket.on('data', (data) => {
-            console.log('Received over TLS:\n', data.toString());
-            // Process server responses and send further commands as needed
-            if (messages.length > 0) { // Check for completion of EHLO response
-                // Now proceed to authenticate or send MAIL FROM, etc.
-                const message = messages.shift();
-                console.log(`Sending: ${message}`);
-                tlsSocket.write(message);
-            }
-        });
-
-        tlsSocket.on('close', () => {
-            console.log('TLS Connection closed');
-        });
-
-        tlsSocket.on('error', (err) => {
-            console.error('TLS Socket Error:', err);
-        });
-    }
-    else {
-        /*if(messages.length !== 0) {
-            const message = messages.shift();
-            console.log(message);
-            client.write(message);
-        }*/
+        upgradeToTLS(client);
+    } else if (hasTLS === false) {
+        handleServerResponse(client, data.toString());
     }
 });
 
@@ -98,3 +59,48 @@ client.on('close', () => {
 client.on('error', (err) => {
     console.error(`Connection error: ${err.message}`);
 });
+
+function handleServerResponse(socket, response) {
+    if (messages.length > 0) { // Check for completion of EHLO response
+        // Now proceed to authenticate or send MAIL FROM, etc.
+        const message = messages.shift();
+        console.log(`Sending: ${message}`);
+        socket.write(message);
+    }
+}
+
+function upgradeToTLS(socket) {
+    const secureContext = tls.createSecureContext({minVersion: 'TLSv1.2'});
+    const tlsSocket = new tls.TLSSocket(socket, {
+        secureContext,
+        isServer: false
+    });
+
+    tlsSocket.connect({host: remoteAddress, port: port}, () => {
+        console.log("Start connect");
+        tlsSocket.write('EHLO localhost\r\n');
+        messages.unshift(`AUTH LOGIN\r\n`,
+            `${Buffer.from(process.env.GG_USERNAME).toString('base64')}\r\n`,
+            `${Buffer.from(process.env.GG_PASSWORD).toString('base64')}\r\n`);
+    });
+    if (!tlsSocket.encrypted) {
+        console.log("Start TLS encryption");
+        tlsSocket.start();
+    }
+    tlsSocket.once('secureConnect', () => {
+        console.log('TLS connection secured');
+        tlsSocket.write('EHLO localhost\r\n');
+    });
+
+    tlsSocket.on('data', (tlsData) => {
+        console.log('Received over TLS:\n', tlsData.toString());
+        handleServerResponse(tlsSocket, tlsData.toString());
+    });
+    tlsSocket.on('close', () => {
+        console.log('TLS Connection closed');
+    });
+
+    tlsSocket.on('error', (err) => {
+        console.error('TLS Socket Error:', err);
+    });
+}
