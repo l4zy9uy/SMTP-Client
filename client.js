fe71db762a087ba8c17200cc9d891a2f626d20ee
l4zy9uy <hoanghuudon02hp@gmail.com>
2024-06-22 17:07:39 +0700

    feat: checking valid message

--- client.js
+++ client.js
@@ -5,20 +5,6 @@ const Logger = require('./Logger');
 const remoteAddress = process.argv[2];
 const senderEmail = process.argv[3];
 const receiverEmail = process.argv[4];
-const subject = "Test Email";
-const port = process.env.PORT_GG;
-let hasTLS = false;
-let mime = {
-    html: 'text/html',
-    txt: 'text/plain',
-    css: 'text/css',
-    gif: 'image/gif',
-    jpg: 'image/jpeg',
-    png: 'image/png',
-    svg: 'image/svg+xml',
-    js: 'application/javascript'
-};
-
 if (!remoteAddress || !receiverEmail || !senderEmail) {
     console.log("You need to pass " +
         "remote Address, " +
@@ -26,34 +12,35 @@ if (!remoteAddress || !receiverEmail || !senderEmail) {
         "receiver Email");
     process.exit(1);
 }
+const port = process.env.PORT_GG;
+let hasTLS = false;
 
-let dataMessage;
+
 let messages;
 const client = new net.Socket();
 const image = fs.readFileSync('cat.jpeg').toString('base64');
-console.log(`size of image: ${image.length}`);
 fs.readFile('./message', (err, data) => {
     if(err) {
         console.error(err);
         return;
     }
-    dataMessage = data.toString('utf-8');
+    if(!isValidEmailMessage(data.toString('utf-8'))) {
+        console.log("Email is not valid");
+        process.exit(1);
+    }
     messages = [
         `EHLO localhost\r\n`,
-        `MAIL FROM:<${senderEmail}> BODY=8BITMIME\r\n`,
+        `MAIL FROM:<${senderEmail}> \r\n`,
         `RCPT TO:<${receiverEmail}>\r\n`,
         `DATA\r\n`,
-        `Subject: ${subject}
-    \r\nFrom: ${senderEmail}
-    \r\nTo: ${receiverEmail}
-    \r\n${data}
+        `${data}
     \r${image}
     \r\n--boundary_9876--
     \r\n.\r\n`,
         `QUIT\r\n`
     ];
 
-    client.connect(port, remoteAddress, () => {
+    client.connect(port, remoteAddress,  () => {
         Logger.info(`Connected to ${remoteAddress}:${port}`);
     });
 });
@@ -121,4 +108,46 @@ function upgradeToTLS(socket) {
     tlsSocket.on('error', (err) => {
         console.error('TLS Socket Error:', err);
     });
+}
+
+function isValidEmailMessage(message) {
+    const lines = message.split('\n');
+
+    // Check for minimum line requirements
+    if (lines.length < 3) {
+        console.log("Incomplete message. Ensure From, To, and Subject lines are included.");
+        process.exit(1);
+    }
+
+    // Destructure the lines array
+    const [fromLine, toLine, subjectLine] = lines;
+
+    // Validate each part
+    validateLine(fromLine, "From");
+    validateLine(toLine, "To");
+    validateLine(subjectLine, "Subject", false);
+
+    return true;
+}
+
+function validateLine(line, type, checkEmail = true) {
+    const parts = line.split(' ');
+
+    if (parts[0] !== `${type}:`) {
+        console.log(`Need to include ${type} part.`);
+        process.exit(1);
+    }
+
+    if (checkEmail && !isValidEmail(parts[1])) {
+        console.log(`The ${type.toLowerCase()} email is invalid.`);
+        process.exit(1);
+    }
+}
+
+function isValidEmail(email) {
+    return String(email)
+        .toLowerCase()
+        .match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        );
 }
---
+++ mimeType.js
@@ -0,0 +1,10 @@
+module.exports.mimeMap = new Map([
+    ['html', 'text/html'],
+    ['txt', 'text/plain'],
+    ['css', 'text/css'],
+    ['gif', 'image/gif'],
+    ['jpg', 'image/jpeg'],
+    ['png', 'image/png'],
+    ['svg', 'image/svg+xml'],
+    ['js', 'application/javascript']
+]);
